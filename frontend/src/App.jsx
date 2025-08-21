b7a2690166415d88a438f2adcdc9e2bab095e2cd
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-08-21 18:33:18 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeTable from "./components/RecipeTable";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe App</h1>
+      <RecipeTable />
+    </div>
+  );
+}
+
+export default App;
